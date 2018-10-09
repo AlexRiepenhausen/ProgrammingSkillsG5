5d494c0ab392b9dbc76f50ce88673900b22798a9
PC <PC@172.20.103.112>
2018-10-09 11:44:29 +0100

    Input part finished - the landscape (template!) can now be initialised with required values. The landscape also takes the input from the landscape files to create the grid

--- ProgrammingSkillsG5/src/application/Main.java
+++ ProgrammingSkillsG5/src/application/Main.java
@@ -5,10 +5,26 @@
 
 package application;
 
+import input.Parser;
+import world.Landscape;
+
 public class Main {
 	
     public static void main(String[] args) {
-        System.out.println("This is a template");
+    	
+    	
+    	/* initalise new landscape with parameters from command line */
+    	//Landscape landscape = Parser.createLandscapeFromCommandLine();
+    	
+    	/* Alternative: initialise new landscape manually (testing purposes) */
+    	Landscape landscape = new Landscape(1.0,1.0,1.0,1.0,1.0,1.0,1.0); 
+    	
+    	/* take the landscape and populate its landscape grid */
+    	Parser.populateLandscapeGridfromFile(landscape,"sample.dat");
+    	
+    	/* print the landscape */
+    	landscape.printLandscape();  
+    	
     }
     
 }
