855af7644ae8366d9a2567adf08994c3a698b351
Hassan Al Marhoon <hhoomm@gmail.com>
2018-11-05 22:38:39 +0000

    Adding the read from config

--- ProgrammingSkillsG5/src/main/java/application/Main.java
+++ ProgrammingSkillsG5/src/main/java/application/Main.java
@@ -11,28 +11,60 @@ import main.java.operation.*;
 
 public class Main {
 	
+	public static final String CONFIG_ARGS = "-config";
+	public static final String FILE_ARGS = "-file";
+	
+	/**
+	 * CONFIG_FILE structure 
+	 * =====================
+	 * input_file:src/main/resources/islands.dat
+	 * birth_rate_hares:0.08
+	 * predation_rate:0.04
+	 * birth_rate_pumas_per_hare_eaten:0.02
+	 * mortality_rate_pumas:0.06
+	 * diffusion_rate_hares:0.2
+	 * diffusion_rate_pumas:0.2
+	 * size_time_step:0.4
+	 * 
+	 * =====================
+	 *  
+	 * @param args -config CONFIG_FILE or -file INPUT_FILE
+	 * @throws Exception
+	 */
     public static void main(String[] args) throws Exception {
+    	Landscape landscape = null;
+    	String configFile = ""; 
+    	if (args.length == 1) {
+    		throw new IllegalArgumentException("Invalid Arguments!");    	
+    	} else if (args.length > 1) {
+    		if (args[0].equalsIgnoreCase(CONFIG_ARGS)) {
+    			/* if the config file specified read the configuration */ 
+    			configFile = args[1];
+    			landscape = Parser.createLandscapefromConfigFile(configFile);
+    			
+    		} else if (args[0].equalsIgnoreCase(FILE_ARGS)) {
+    			/* if the input file specified run with the default parameters */
+    			landscape = new Landscape(); 
+    			Parser.populateLandscapeGridfromFile(landscape,args[1]);
+    		}
+    		
+    	} else {
+    		/* if no argument specified initalise new landscape 
+    		 * with parameters from command line */
+        	landscape = Parser.createLandscapeFromCommandLine();	
+    	}
     	
-    	/* initalise new landscape with parameters from command line */
-    	//Landscape landscape = Parser.createLandscapeFromCommandLine();
-    	
-    	/* Alternative: initialise new landscape manually (testing purposes) */
-    	Landscape landscape = new Landscape(); 
-    	
-    	/* take the landscape and populate its landscape grid */
-    	Parser.populateLandscapeGridfromFile(landscape,"islands.dat");
-    	    	
-    	/* print the landscape */
-    	//landscape.printLandscape();
-    
-    	
-
-    	
+    	if (landscape == null) {
+    		System.err.println("The application terminated unsuccessfully");
+    		return;
+    	}
+    		
+    		
 		Out.printOutput("start");
     	
     	/* print the landscape */
     	
-    	for (int i=0; i<50; i++) {
+    	for (int i=0; i<5; i++) {
     		Processor.run(landscape, 10);    		
     		Out.generatePlainPPMFile(landscape, i);
     	}	
@@ -46,8 +78,6 @@ public class Main {
 		//Out.createTempFile(500, 400);
 		
     	Out.printOutput("finish");
-    	
-    	
 
     	
     }
--- ProgrammingSkillsG5/src/main/java/input/Parser.java
+++ ProgrammingSkillsG5/src/main/java/input/Parser.java
@@ -12,6 +12,17 @@ import main.java.world.Landscape;
 
 public class Parser {
 	
+	// Config File constants
+	public static final String CONFIG_INPUT_FILE = "input_file";
+	public static final String CONFIG_BIRTH_RATE_HARES = "birth_rate_hares";
+	public static final String CONFIG_PREDATION_RATE = "predation_rate";
+	public static final String CONFIG_BIRTH_RATE_PUMAS_PER_HARE_EATEN = "birth_rate_pumas_per_hare_eaten";
+	public static final String CONFIG_MORTALITY_RATE_PUMAS = "mortality_rate_pumas";
+	public static final String CONFIG_DIFFUSION_RATE_HARES = "diffusion_rate_hares";
+	public static final String CONFIG_DIFFUSION_RATE_PUMAS = "diffusion_rate_pumas";
+	public static final String CONFIG_SIZE_TIME_STEP = "size_time_step";
+	
+	
 	public static BufferedReader b_read;
 	
 	/**
@@ -21,7 +32,7 @@ public class Parser {
 	public static void initBuffRead(String file_name) throws FileNotFoundException {
 		
 		System.out.println("Reading file ...");
-		File landscape_file = new File("src/main/resources/" + file_name);	
+		File landscape_file = new File(file_name);	
 		FileReader f_read   = new FileReader(landscape_file);
 		
 		b_read = new BufferedReader(f_read);	
@@ -75,6 +86,85 @@ public class Parser {
 							 dbl_inputs[5],
 							 dbl_inputs[6]);
 							 
+	}
+	
+	public static Landscape createLandscapefromConfigFile(String configFile) {
+		String line = null;		
+		BufferedReader config_b_read = null;
+		Landscape landscape = null;
+		
+		try {
+			System.out.println("Reading config file ...");
+			File config_file = new File(configFile);	
+			FileReader f_read   = new FileReader(config_file);
+			config_b_read = new BufferedReader(f_read);	
+			
+		} catch (FileNotFoundException e) {
+			System.err.println(e.getLocalizedMessage());
+			return null;
+		}
+
+		try {
+			String[] values;
+			String inputFilename = null;
+			double birth_rate_hares = 0;
+			double predation_rate = 0;		
+			double birth_rate_pumas_per_hare_eaten = 0;
+			double mortality_rate_pumas = 0;
+			double diffusion_rate_hares = 0;
+			double diffusion_rate_pumas = 0;
+			double size_time_step = 0;
+			/*
+
+			public final String CONFIG_SIZE_TIME_STEP = "size_time_step";
+			*/
+			
+			while((line = config_b_read.readLine()) != null) {
+				values = line.split(":");
+				if (values.length == 2) {
+					if (values[0].equalsIgnoreCase(CONFIG_INPUT_FILE)) {
+						inputFilename = values[1];
+					} else if (values[0].equalsIgnoreCase(CONFIG_BIRTH_RATE_HARES)) {
+						birth_rate_hares = Double.parseDouble(values[1]);
+					} else if (values[0].equalsIgnoreCase(CONFIG_PREDATION_RATE)) {
+						predation_rate = Double.parseDouble(values[1]);
+					} else if (values[0].equalsIgnoreCase(CONFIG_BIRTH_RATE_PUMAS_PER_HARE_EATEN)) {
+						birth_rate_pumas_per_hare_eaten = Double.parseDouble(values[1]);
+					} else if (values[0].equalsIgnoreCase(CONFIG_MORTALITY_RATE_PUMAS)) {
+						mortality_rate_pumas = Double.parseDouble(values[1]);
+					} else if (values[0].equalsIgnoreCase(CONFIG_DIFFUSION_RATE_HARES)) {
+						diffusion_rate_hares = Double.parseDouble(values[1]);
+					} else if (values[0].equalsIgnoreCase(CONFIG_DIFFUSION_RATE_PUMAS)) {
+						diffusion_rate_pumas = Double.parseDouble(values[1]);
+					} else if (values[0].equalsIgnoreCase(CONFIG_SIZE_TIME_STEP)) {
+						size_time_step = Double.parseDouble(values[1]);
+					} 
+				}else {
+					System.err.println("Invalid config file!");
+					return null;
+				}
+					
+			}
+			config_b_read.close();
+			
+			if (inputFilename != null) {				
+				landscape = new Landscape(birth_rate_hares,
+								predation_rate,
+								birth_rate_pumas_per_hare_eaten,
+								mortality_rate_pumas,
+								diffusion_rate_hares,
+								diffusion_rate_pumas,
+								size_time_step);
+				
+				Parser.populateLandscapeGridfromFile(landscape, inputFilename);
+				return landscape;
+			}
+			
+		} catch (IOException e) {
+			System.err.println(e.getLocalizedMessage());
+			return null;
+		}		
+		return null;
 	}
 	
 	/**
